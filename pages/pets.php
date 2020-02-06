01df876fde3d02377910e4555360a291add6a32b
Malcolm Diller <malcolm@dillerm.io>
2020-02-05 20:29:00 -0800

    added pets section

---
+++ pages/pets.php
@@ -0,0 +1,21 @@
+
+
+<?php foreach($json_pet_infos as $pet) { ?>
+<div class="pet">
+<img src="images/face.jpg" />
+<h3><?php echo $pet['name']; ?></h3>
+<?php echo parseAge($pet['born']); ?> Old<br>
+Job: <?php echo $pet['job']; ?><br>
+<p>
+<?php echo $pet['description']; ?>
+</p>
+<p>
+	Qualities
+	<ul>
+		<?php foreach($pet['qualities'] as $quality) { ?>
+			<li><?php echo $quality; ?></li>
+		<?php } ?>
+	</ul>
+</p>
+</div>
+<?php } ?>
--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,41 @@
 <?php
 $json_page_infos = json_decode(file_get_contents("pages_info.json"), true);
+$json_pet_infos = json_decode(file_get_contents("pets_info.json"), true);
 
 define("SITE_URL", "http://" . $_SERVER['SERVER_NAME']);
 
 function yearsSince($then) {
 	return date("Y") - $then;
+}
+
+function pluralize($value, $text) {
+	if ($value == 0) {
+		return "";
+	}
+	elseif ($value == 1) {
+		return $value . $text;
+	}
+	else {
+		return $value . $text . "s";
+	}
+}
+
+function parseAge($birth) {
+	$birth = DateTime::createFromFormat("M Y", $birth);
+	$today = new DateTime("now");
+	$diff = $birth->diff($today);
+	$diff_months = pluralize($diff->m, " Month");
+	$diff_years = pluralize($diff->y, " Year");
+
+	if ($diff->y == 0) {
+		return $diff_months;
+	}
+	elseif ($diff->y <= 1 and $diff->m != 0) {
+		return $diff_years . " and " . $diff_months;
+	}
+	else {
+		return $diff_years;
+	}
 }
 
 ?>
