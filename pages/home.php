6b7d4532d28aaedbc28d8b931ba44ce0eb5775de
Malcolm Diller <malcolm@dillerm.io>
2020-02-13 18:46:00 -0800

    fixed formatting and updated plant names

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,6 @@
-
-
 <p>
-Example text here. Introductory stuff.
+	Amanda is amazing. Shes a compassionate, beautiful, super-rad woman who brings out the best in people. This website is a tribute to her.
+</p>
+<p>
+	<b>Note to Amanda:</b> The other half of this present is I'll update this to be whatever you want! This is just what I thought of to add so far.
 </p>
